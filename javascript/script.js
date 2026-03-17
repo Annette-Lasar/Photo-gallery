438ab294f149f892728e17bf9e61614a461dcc6d
Annette Lasar <146415092+Annette-Lasar@users.noreply.github.com>
2026-03-17 17:42:36 +0100

    Push template functions into separate file

--- javascript/script.js
+++ javascript/script.js
@@ -15,17 +15,6 @@ function renderMyPictures() {
   }
 }
 
-<<<<<<< HEAD
-function generateGalleryHTML(picture, i) {
-  return /* html */ `
-    <figure class=${picture.imageClass} onclick="openImage(${i})">
-      <img src="${picture.picture}" alt="">
-  </figure>
-  `;
-}
-
-=======
->>>>>>> 1297012cfc9193ac7476789d42e22ed590851a08
 /* defines the function openImage(i)
 on clicking on a photo a detailed view is shown */
 function openImage(i) {
@@ -36,27 +25,6 @@ function openImage(i) {
   dialogBox.classList.remove('d-none');
 }
 
-<<<<<<< HEAD
-function generatePhotoBoxHTML(i, picture) {
-  return /*html*/ `
-  <div id="photo-box" class="photo-box"> 
-      <figure>
-      <img src="${picture.picture}" alt="" />
-      <figcaption>${picture.caption}</figcaption>
-      </figure>
-          <div onclick="closeImage()" class="close-button">
-            <img src="../assets/x-close.png" alt="" />
-          </div>
-          <div class="arrows">
-            <img onclick="showPreviousPicture(${i})" src="../assets/arrow-left.svg" alt="" width="48" />
-            <img onclick="showNextPicture(${i})" src="../assets/arrow-right.svg" alt="" width="48" />
-          </div>
-    </div>
-  `;
-}
-
-=======
->>>>>>> 1297012cfc9193ac7476789d42e22ed590851a08
 /* closes the image box that displays a detailed view of one photo */
 function closeImage() {
   let closedImage = document.getElementById('dialog-bg');
